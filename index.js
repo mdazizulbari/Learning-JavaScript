f25e153901b4e64300211aab37c7d63c2e2ff667
SMA Bari <mdazizulbarishafin@gmail.com>
2024-07-08 11:14:35 +0600

    varriables

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-console.log(`Hello`)
-window.alert(`this is an aleart`)
+// console.log(`Hello`);
+// window.alert(`this is an aleart`);
 
 /* this 
 is a 
 comment */
 
-document.getElementById(`myh1`).textContent = `hello`
+document.getElementById(`myh1`).textContent = `hello`;
+
+// let x;
+// x = 100;
+// console.log(x);
+
+// let price = 10.99;
+// console.log(`The price is $${price}`);
+// console.log(typeof price);
+
+// let email = `bro123@gmail.com`;
+// console.log(typeof email);
+// console.log(`Your email is ${email}`);

971639c8a05938d8f028e61f679f6237e22c6ce5
SMA Bari <mdazizulbarishafin@gmail.com>
2024-07-08 13:19:07 +0600

    Arithmetic Operators

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 is a 
 comment */
 
-document.getElementById(`myh1`).textContent = `hello`;
+// document.getElementById(`myh1`).textContent = `hello`;
 
 // let x;
 // x = 100;
@@ -18,3 +18,33 @@ document.getElementById(`myh1`).textContent = `hello`;
 // let email = `bro123@gmail.com`;
 // console.log(typeof email);
 // console.log(`Your email is ${email}`);
+
+// let online = false;
+// console.log(`Bro is online: ${online}`);
+// let forSale = true;
+// console.log(`Is the car for sale: ${forSale}`);
+
+// Varriables
+let fullname = `Sma Bari`;
+let age = 19;
+let isStudent = true;
+document.getElementById(`p1`).textContent = `Bro's name is ${fullname}`;
+document.getElementById(`p2`).textContent = `Bro's age is ${age}`;
+document.getElementById(`p3`).textContent = `Bro is a student: ${isStudent}`;
+
+// Arithmetic Operators
+let students = 30;
+students = students + 1;
+students += 1;
+// students ++;
+// All 3 lines are the same
+students -= 1;
+students --;
+students *= 2;
+students /= 2;
+// To the power
+students **= 2;
+// The remainder of devision
+// let extrastudents = students % 3;
+// students %= 2;
+console.log(`The number of students is ${students}`)
